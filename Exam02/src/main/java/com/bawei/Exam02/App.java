60c3ee199aa5fa2ae7c1831bcb9fe8ebe3437197
chen <2954479033@qq.com>
2019-04-16 15:32:09 +0800

    XiangMu01

---
+++ Exam02/src/main/java/com/bawei/Exam02/App.java
@@ -0,0 +1,13 @@
+package com.bawei.Exam02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
